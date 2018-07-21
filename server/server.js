3bedbcf0e59e268467f08d9a031c4f6b13bf98dc
pyszny <maciej.sawicky@gmail.com>
2018-07-21 14:19:45 +0200

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,9 +22,19 @@ app.post('/todos', (req, res) => {
     })
 });
 
-app.listen(3007, () => {
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
     console.log('Server started');
 });
 
 
 module.exports = {app};
+
+
