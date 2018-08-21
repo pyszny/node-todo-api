ad1c2e7f5cf357a7ed43dc449f63a6eeaa277bc6
pyszny <maciej.sawicky@gmail.com>
2018-08-21 22:08:36 +0200

    Set up separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;                                      // setting mongoose to use promises
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');                  // setting mongoose db connection
+mongoose.connect(process.env.MONGODB_URI);                  // setting mongoose db connection
 
 module.exports = {mongoose};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
     text: 'First test todo'
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -139,5 +141,50 @@ describe('DELETE /todos/:id', () => {
             .end(done);
     });
 
+});
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        let hexId = todos[0]._id.toHexString();
+        let text = 'patch text';
+
+        request(app)                                                // first send request
+            .patch(`/todos/${hexId}`)                               // type patch
+            .send({                                                 // specify data
+                completed: true,
+                text
+            })
+            .expect(200)
+            .expect((res) => {                                      // expects response
+                expect(res.body.todo.text).toBe(text);              // with specified data
+                expect(res.body.todo.completed).toBe(true);
+                expect(res.body.todo.completedAt).toBeNumber;
+            })
+            .end(done);
+        });
+
+
+
+
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        let hexId = todos[1]._id.toHexString();
+        let text = 'patch changed text';
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .expect(200)
+            .send({
+                completed: false,
+                text
+            })
+            .expect((res) => {
+                expect(res.body.todo.completed).toBe(false);
+                console.log(res);
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completedAt).toNotExist;
+            })
+            .end(done);
+    });
 });
 
