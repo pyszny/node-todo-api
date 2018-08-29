b40232e609180d0e1cd3559249fad588c9a25a36
pyszny <maciej.sawicky@gmail.com>
2018-08-29 23:41:22 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -109,6 +109,18 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.genereteAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
 });
